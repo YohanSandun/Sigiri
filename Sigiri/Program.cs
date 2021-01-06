14738681068ba4c7d43f0421b6be65cb8b485096
yohan sandun <yohan99ysk@gmail.com>
2021-01-06 11:25:28 +0530

    Initial commit

---
+++ Sigiri/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sigiri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
